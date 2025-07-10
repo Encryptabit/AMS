3d02ad2808632b50d579b8d2e61909f78df1776c
Jacari Jones <carithecoder@gmail.com>
2025-07-09 22:21:03 -0700

    init

---
+++ AMS/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AMS.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ AMS/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AMS.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
