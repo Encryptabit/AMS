844e7adf95fc2ee40260fb9205d3de5a3c317384
Jacari Jones <jjones@autocrib.com>
2025-11-05 16:11:16 -0800

    encoding is hard

--- host/Ams.Core/Services/Integrations/FFmpeg/FfUtils.cs
+++ host/Ams.Core/Services/Integrations/FFmpeg/FfUtils.cs
@@ -49,4 +49,63 @@ internal static class FfUtils
         ffmpeg.av_channel_layout_default(&layout, channels);
         return layout;
     }
+
+    public static unsafe int CheckSampleFormat(AVCodec* codec, AVSampleFormat format)
+    {
+        AVSampleFormat* p = codec->sample_fmts;
+        if (format == AVSampleFormat.AV_SAMPLE_FMT_NONE)
+        {
+            throw new ArgumentException("Invalid sample format", nameof(format));
+        }
+
+        return 0;
+    }
+
+    public static unsafe int SelectSampleRate(AVCodecContext* ctx)
+    {
+        int* supportedSamplerates;
+        int i, numSamplerates, ret;
+        
+        ret = ffmpeg.avcodec_get_supported_config(ctx, null, AVCodecConfig.AV_CODEC_CONFIG_SAMPLE_RATE,
+            0, (void**)&supportedSamplerates, &numSamplerates);
+
+        // return error
+        if (ret < 0) return ret;
+        
+        if (supportedSamplerates is null)
+        {
+            return 44100;
+        }
+
+        for(i = 0; i < numSamplerates; i++)
+        {
+            if(ctx->sample_rate == supportedSamplerates[i])
+            {
+                return supportedSamplerates[i];
+            }
+
+            if (i == numSamplerates)
+            {
+                ffmpeg.av_log(ctx, ffmpeg.AV_LOG_ERROR, $"Specified sample rate {ctx->sample_rate} is not supported by the {ctx->codec->name->ToString()} encoder\n");
+            } 
+
+        }
+
+        return 0;
+    }
+
+    public static unsafe AVChannelLayout SelectChannelLayout(AVCodecContext* ctx)
+    {
+        
+        // short circuit for now, we dont have to support this yet
+        return CreateDefaultChannelLayout(1); 
+        
+        ulong* p;
+        AVChannelLayout* chLayouts;
+        int numChLayouts = 0;
+        
+        ffmpeg.avcodec_get_supported_config(ctx, null, AVCodecConfig.AV_CODEC_CONFIG_CHANNEL_LAYOUT,
+            0, (void**)&chLayouts, &numChLayouts);
+
+    }
 }
