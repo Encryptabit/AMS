d2ed6c89cf13dfd6ff945f90ae59b5630f7c0ba8
Jacari Jones <jjones@autocrib.com>
2025-11-19 14:28:06 -0800

    remove mudblazor and old blazor project, we're about to get ... fast

--- host/Ams.Web/Requests/ExportSentenceRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace Ams.Web.Requests;
-
-public sealed record ExportSentenceRequest
-{
-    public string? ErrorType { get; init; }
-    public string? Comment { get; init; }
-}
--- host/Ams.Web/Requests/WorkspaceUpdateRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace Ams.Web.Requests;
-
-public sealed record WorkspaceUpdateRequest
-{
-    public string? WorkspaceRoot { get; init; }
-    public string? BookIndexPath { get; init; }
-    public string? CrxTemplatePath { get; init; }
-}
